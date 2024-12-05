dbb09bf6861b1d3227f5dcd9a3a7891f2ecea430
Viran <venuraka.20191253@iit.ac.lk>
2024-12-05 09:36:17 +0530

    Added the currency convertor

---
+++ currency_converter_app/lib/config/color_codes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorCodes {
+  Color color1 = Color.fromRGBO(0, 0, 0, 1);
+  Color color2 = Color.fromRGBO(29, 29, 29, 1);
+  Color color3 = Color.fromRGBO(255, 255, 255, 1);
+  Color color4 = Color.fromRGBO(62, 131, 30, 1);
+  Color color5 = Color.fromRGBO(80, 80, 80, 1);
+}
